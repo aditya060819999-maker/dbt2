f11deba790353e7182687823e0d978f436c64747
Aditya Aditya <aditya06081999gedam@gmail.com>
2026-06-14 15:36:45 +0000

    comoit

---
+++ models/incremental.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='incremental',
+    unique_key='order_id',
+    incremental_strategy='merge'
+) }}
+
+select
+    order_id,
+    product,
+    amount,
+    order_datetime,
+    '{{ invocation_id }}' as dbt_invocation_id
+from {{ source('src', 'sometable') }}
+
+{% if is_incremental() %}
+where order_datetime >
+(
+    select max(order_datetime)
+    from {{ this }}
+)
+{% endif %}
